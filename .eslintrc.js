bbd3ea72f5a29f9942c54fd5315793e2b98dd0e4
WebCrazy003 <raycoaster99001@gmail.com>
2025-03-01 18:48:59 -0500

    fix: fix eslint errors

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'airbnb', // Base Airbnb configuration
+    'prettier', // Prettier configuration (turns off conflicting rules)
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'], // Add the Prettier plugin
+  rules: {
+    'prettier/prettier': 'error', // Enforce Prettier formatting
+    'react/react-in-jsx-scope': 'off', // Disable this rule in React 17+
+    'react/jsx-filename-extension': 'off',
+    'no-plusplus': 'off',
+    'react/prop-types': 'off',
+    'react/no-array-index-key': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+  },
+};
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,15 @@ import {
   Stack,
   Text,
   useColorModeValue,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
 export default function Footer() {
   return (
     <Box
-      bg={useColorModeValue("gray.50", "gray.900")}
-      color={useColorModeValue("gray.700", "gray.200")}
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}
     >
-      <Container
-        as={Stack}
-        maxW={"6xl"}
-        py={4}
-        align="center"
-      >
+      <Container as={Stack} maxW="6xl" py={4} align="center">
         <Text>© 2023 Eldora Boo. All rights reserved</Text>
       </Container>
     </Box>
